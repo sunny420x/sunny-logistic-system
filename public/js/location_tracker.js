71a9b5c8f82eb0faafcbd9c4480a0557b161314e
Sunny Jirakit <jirakitpawnsakunrungrot@gmail.com>
2026-08-11 08:35:33 +0700

    feat: location tracker js update

--- public/js/location_tracker.js
+++ public/js/location_tracker.js
@@ -3,38 +3,37 @@ let position_longitude;
 let watchId = null;
 let locationIntervalId = null;
 
-
 function handleNewPosition(lat, lon) {
   if (position_latitude !== lat || position_longitude !== lon) {
     position_latitude = lat;
     position_longitude = lon;
     
-    console.log(`📍 [Location Updated] Lat: ${lat}, Lon: ${lon}`);
+    console.log(`📍 [Saved Location] Lat: ${lat}, Lon: ${lon}`);
 
     if (typeof loadMyRoute === 'function') {
       loadMyRoute();
     }
 
-    sendLocationToServer(position_latitude, position_longitude);
+    sendLocationToServer();
   }
 }
 
 function startLocationTracking() {
   if (window.LocationChannel) {
-    console.log('📱 [System]: ตรวจพบการทำงานบน Flutter WebView');
+    console.log('[+] Using Mobile App Location Channel.');
 
     window.LocationChannel.postMessage('requestLocation');
 
     locationIntervalId = setInterval(() => {
       if (window.LocationChannel) {
         window.LocationChannel.postMessage('requestLocation');
-        console.log('🔄 [JS -> Flutter] สะกิดขอพิกัดรอบประจำ');
+        console.log('[+] Requesting Location From Mobile App.');
       }
     }, 20000);
 
   } 
   else if (navigator.geolocation) {
-    console.log('🌐 [System]: ตรวจพบการทำงานบน Web Browser ปกติ');
+    console.log('[+]: Using Web Browser Location Service.');
 
     const options = {
       enableHighAccuracy: true,
@@ -47,13 +46,13 @@ function startLocationTracking() {
         handleNewPosition(position.coords.latitude, position.coords.longitude);
       },
       (error) => {
-        console.error('❌ Browser Geolocation Error:', error.message);
+        console.error('[!] Browser Geolocation Error:', error.message);
       },
       options
     );
 
   } else {
-    console.error('❌ เบราว์เซอร์นี้ไม่รองรับการดึงพิกัด Geolocation');
+    console.error('[!] Geolocation is not supported Browser not support');
   }
 }
 
@@ -62,19 +61,19 @@ function sendLocationToServer(lat, lon) {
   if (typeof truck_id === 'undefined' || typeof driver_id === 'undefined' || !truck_id || !driver_id || !lat || !lon) {
     return;
   }
-
-  fetch(`/api/saveLocation/${truck_id}/${driver_id}/${lat}/${lon}`)
+  //Using Global Variables.
+  fetch(`/api/saveLocation/${truck_id}/${driver_id}/${position_latitude}/${position_longitude}`)
     .then(response => {
       if (!response.ok) {
-        throw new Error(`เซิร์ฟเวอร์ตอบกลับด้วยสถานะ: ${response.status}`);
+        throw new Error(`Server Response with: ${response.status}`);
       }
       return response.json();
     })
     .then(data => {
-      console.log("📥 ข้อมูลตอบกลับจากเซิร์ฟเวอร์:", data);
+      console.log("Server Response Data:", data);
     })
     .catch(error => {
-      console.error("❌ เกิดข้อผิดพลาดในการ Fetch ข้อมูล:", error);
+      console.error("[!] Fetch saveLocation API Error:", error);
     });
 }
 
